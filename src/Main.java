5e4a702ec005141bf4c6a0a09043f4946dbbcc32
Marker97 <chereche10@gmail.com>
2022-11-12 16:56:10 +0100

    If Statement_01

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String myName = "Marcus";
+
+        if(myName.length() % 2 == 0){
+            System.out.println("The number of letters in your name is even.");
+        }else {
+            System.out.println("The number of letters in your name is odd.");
+        }
+    }
+}
